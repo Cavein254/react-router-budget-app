b50c98676ddcf2a9111d97663a86825757e25c99
johnmuthua <caveomuthama@gmail.com>
2024-02-02 18:47:48 +0300

    Add intial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home Page</h1>,
+  },
+  {
+    path: "/:1d",
+    element: <h1>Home id</h1>,
+  },
+]);
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
