96718442ef92610cef78ab874ae95c78a028510d
johnmuthua <caveomuthama@gmail.com>
2024-02-02 20:17:22 +0300

    Add form

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import logoMark from "../assets/logomark.svg";
+import { Form, NavLink } from "react-router-dom";
 
 const Nav = ({ userName }) => {
   return (
     <nav>
-      <img src={logoMark} alt="logo" height={40} />
-      <span>Home Budget</span>
+      <NavLink to="/" aria-label="Go to Home">
+        <img src={logoMark} alt="logo" height={40} />
+        <span>HomeBudget</span>
+      </NavLink>
+      {userName && (
+        <Form method="POST" action="/logout">
+          <button className="btn btn--warning" type="submit">
+            Delete User
+          </button>
+        </Form>
+      )}
     </nav>
   );
 };
